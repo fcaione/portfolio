672d2e74e47d56f41d6716a32f02c04c5c60e5f2
Freddy Caione <fac299@nyu.edu>
2023-04-12 16:35:00 -0400

    projects.js data

---
+++ src/data/Projects.js
@@ -0,0 +1,9 @@
+const projects = [
+  {
+    "title": "Mancala",
+    "description": "Classic mancala with a cozy/8-bit theme",
+    "tools": ["Javascript", "HTML", "CSS"],
+    "github": "https://github.com/fcaione/Mancala",
+    "deployed": "https://cozy-mancala.surge.sh/"
+  },
+]
