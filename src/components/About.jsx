d0b553c2cffbd256080780797f34de8862f86be2
Freddy Caione <fac299@nyu.edu>
2023-04-24 18:30:32 -0400

    about page

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+const About = () => {
+	return (
+		<>
+			<div>
+				I am an analytically-driven software engineer interested in the intersection between rhetoric and coding.
+			</div>
+			<div>
+				Recently, I took the leap to further my interest in software development at General Assembly.
+			</div>
+			Ï
+		</>
+	)
+}
+export default About
