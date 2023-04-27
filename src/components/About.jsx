3c15da67b4abc0b626fbe5a20a60dd412ba4cd87
Freddy Caione <fac299@nyu.edu>
2023-04-27 13:49:16 -0400

    edited bio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,7 @@ const About = () => {
 				<div className="flex flex-col md:flex-row justify-center items-center gap-20">
 					<img className="w-80" src={pfp} alt="" />
 					<div>
-						I am an analytically-driven software engineer interested in the
-						intersection between rhetoric and coding.
+					As a software engineer, I am passionate about using technology to create solutions that connects people and enhances their lives. With a strong background in effective communication and a love for strategy video games and board games like Chess and Catan, I approach programming as both an art and a science. I believe that by blending technical expertise with a deep understanding of user needs, we can create software that not only functions seamlessly but also delights its audience. My goal is to build programs that solve real-world problems and provide a superior user experience. In my free time, I enjoy competitive video games, updating my letterboxd, and exploring new artists of alternative and hyperpop music.
 					</div>
 				</div>
 			</section>
